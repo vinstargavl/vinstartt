f8e362f3598f9055aad34c0360f7ef99552728f3
vinstargavl <vinstarhiu@gmail.com>
2025-01-05 07:40:37 +0700

    totinh

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    introTitle: 'Chị Hương à!',
+    introDesc: `Chị Hương à , Chị có thích em không,
+    nếu không thì thôi vậy :((
+    đây chỉ là 1 web em code ra em sẽ không biết được chị chọn j
+    nếu chị không đồng ý thì đừng rep lại tin nhắn em kể từ khi ấn vô link.
+    Cảm ơn chị đã cho em biết`,
+    btnIntro: 'Có thích',
+    title: 'Chị Hương có đồng ý làm ny em ko 😙',
+    desc: 'Em biết code nút Không hơi mất dạy nhưng nếu chị không đồng ý thì đừng rep lại tin nhắn em , em sẽ tự hiểu :(',
+    btnYes: 'ok em <33',
+    btnNo: 'Không',
+    question:'Cho 1 cái lí do :))',
+    btnReply: 'Gửi ',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Em biết mà :)) 🥰',
+    messDesc: '',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/vinstardangcap' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
