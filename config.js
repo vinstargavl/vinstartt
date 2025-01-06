34fd2ca65540325e03d31ea74105935348e0bec8
vinstargavl <vinstarhiu@gmail.com>
2025-01-06 13:46:41 +0700

    Hh

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
     introTitle: 'Chị Hương à!',
     introDesc: `Chị có thích em không ?
-    Nếu chị có thích thì em cũng thích chị 
+    Nếu chị có thích thì em cũng thích chị .
     Nếu chị không thích em thì thôi vậy
     hãy vào mess và cho em biết `,
-    btnIntro: 'Cos',
-    title: 'Chị Hương đồng ý làm ny em không 👉👈',
+    btnIntro: ' Có ',
+    title: 'Chị Hương đồng ý làm ny em không               👉👈',
     desc: 'Em code chữ không hơi mất dạy nhưng quyền lựa chọn của chị ko phải chỉ ở bên trong 1 cái web mà còn ở bên ngoài nữa :(( ',
     btnYes: 'Đồng ý ☺️',
     btnNo: 'Không ',
     question:'Cái j cũng cần lí do :3 ',
     btnReply: 'Gửi ',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'hh',
+    mess: '',
     messDesc: 'Hãy cho biết sự lựa chọn của chị ở trong mess',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/vinstardangcap' //link mess của các bạn. VD: https://m.me/nam.nodemy
