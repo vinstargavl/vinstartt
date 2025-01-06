29ff2798787142d95e23059e5da608508bcbedaf
vinstargavl <vinstarhiu@gmail.com>
2025-01-06 13:25:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,18 +3,17 @@ const CONFIG = {
     introDesc: `Chị Hương à , Chị có thích em không,
     nếu không thì thôi vậy :((
     đây chỉ là 1 web em code ra em sẽ không biết được chị chọn j
-    nếu chị không đồng ý thì đừng rep lại tin nhắn em kể từ khi ấn vô link.
-    Cảm ơn chị đã cho em biết`,
+    nếu chị không đồng ý thì hãy vào mess và cho em biết :((`,
     btnIntro: 'Có thích',
     title: 'Chị Hương có đồng ý làm ny em ko 😙',
-    desc: 'Em biết code nút Không hơi mất dạy nhưng nếu chị không đồng ý thì đừng rep lại tin nhắn em , em sẽ tự hiểu :(',
+    desc: 'Em biết code nút Không hơi mất dạy nhưng nếu chị không đồng ý thì hãy nhắn vào mess đề cho em biết😭 ',
     btnYes: 'ok em <33',
     btnNo: 'Không',
     question:'Cho 1 cái lí do :))',
     btnReply: 'Gửi ',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Em biết mà :)) 🥰',
-    messDesc: '',
+    messDesc: '',0
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/vinstardangcap' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
